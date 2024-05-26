53ce40229effbda5f50f13884aa8b3822c0c1e4f
Akash kumar Nayak <akashnayak3009@gmail.com>
2024-05-26 18:53:24 +0530

    common component making

--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import TicketCard from "./(components)/TicketCard";
+import StatusDisplay from "./(components)/StatusDisplay";
 
 export default function Dashboard() {
   return (
-    <>
-     Dashboard
-    </>
+    <div className="p-5 ">
+      <div className="lg:grid grid-cols-2 xl:grid-cols-4 "> 
+      <TicketCard />
+      <TicketCard />
+      <TicketCard />
+      <TicketCard /> 
+      </div>
+      
+    </div>
   );
 }
